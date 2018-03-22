5a768a5d7488a64b0faa410a80a23fec3a2bd6be
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-03-22 10:52:27 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import org.hibernate.HibernateException;
+import org.hibernate.Metamodel;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.metamodel.EntityType;
+
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
